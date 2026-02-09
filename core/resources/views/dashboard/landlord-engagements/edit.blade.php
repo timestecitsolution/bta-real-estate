8d49111e1f2e0557d4b0f03814a722732e1b4c1f
anik4997 <oliahammed02@gmail.com>
2026-02-09 17:57:42 +0600

    engagement error for null flat id

--- core/resources/views/dashboard/landlord-engagements/edit.blade.php
+++ core/resources/views/dashboard/landlord-engagements/edit.blade.php
@@ -180,6 +180,7 @@
                     </div>
 
                     {{-- FLAT --}}
+                    @if($engagement->flat != null)
                     <div class="form-group row">
                         <label class="col-sm-2 form-control-label">Flat *</label>
                         <div class="col-sm-10">
@@ -190,7 +191,7 @@
                             </select>
                         </div>
                     </div>
-
+                    @endif
                     {{-- ================= FLAT DOCUMENTS ================= --}}
                     <div class="document-wrapper flat-doc-wrapper">
                     @forelse($engagement->flatDocuments as $dIndex => $doc)
